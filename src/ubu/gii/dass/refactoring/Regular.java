4d39142aaa2602cb125063d3f5ca49038be06287
jose.montoya.ramirez.contractor <jose.montoya.ramirez.contractor@bbva.com>
2023-05-15 20:49:39 +0200

    Create Regular Movie subclass

---
+++ src/ubu/gii/dass/refactoring/Regular.java
@@ -0,0 +1,10 @@
+package ubu.gii.dass.refactoring;
+
+public class Regular extends Movie {
+
+	public Regular(String title, int priceCode) {
+		super(title, priceCode);
+		// TODO Auto-generated constructor stub
+	}
+
+}
