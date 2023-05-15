cb3f6472215b9bd72d811c8c392cd9e4d2f5eebb
jose.montoya.ramirez.contractor <jose.montoya.ramirez.contractor@bbva.com>
2023-05-15 20:51:26 +0200

    Create NewRelease Movie subclass

---
+++ src/ubu/gii/dass/refactoring/NewRelease.java
@@ -0,0 +1,10 @@
+package ubu.gii.dass.refactoring;
+
+public class NewRelease extends Movie {
+
+	public NewRelease(String title, int priceCode) {
+		super(title, priceCode);
+		// TODO Auto-generated constructor stub
+	}
+
+}
